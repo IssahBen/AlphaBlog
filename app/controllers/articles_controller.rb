f2edd8919a49192ac01f0bd7a7e495582b0f64e7
IssahBen <benedictissah@gmail.com>
2024-01-23 19:36:51 -0600

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+    def show
+        @articles=Article.find(params[:id])
+    end
+
+    def index 
+        @articles=Article.all
+    end
+
+
+   
+
+
+end
