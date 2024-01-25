734690299feeec2dc6af661d844b747c04a8e611
IssahBen <benedictissah@gmail.com>
2024-01-24 23:06:38 -0600

    update and edit action included

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,44 @@ class ArticlesController < ApplicationController
         @articles=Article.all
     end
 
+    def new
+        @article=Article.new
+    end
 
-   
+    def create
+        @article=Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice]= "article was created"
+            redirect_to @article
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @article=Article.find(params[:id])
+
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice]= "article was created"
+            redirect_to @article
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+
+
+    end
+
+
+    
+
+    def edit
+
+        @article=Article.find(params[:id])
+
+    
+
+    end
+
 
 
 end
